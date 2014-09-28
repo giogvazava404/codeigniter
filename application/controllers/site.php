f4477eb21ed64e67989705f5798aa6d4afc3389d
codeigniter <web.gvazava@gmail.com>
2014-09-28 21:16:05 +0400

    from notebook

---
+++ application/controllers/site.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+
+	public function index()
+	{
+		//$this->load->view('welcome_message');
+		//$this->addStaff( );
+		$this->home();
+	}
+	
+	public function home(){
+		$data["title"] = "Home";		
+		$this->load->view("view_home", $data);
+	}
+	
+	public function about(){
+		$data["title"] = "About";		
+		$this->load->view("view_about", $data);
+	}
+}
---
+++ application/views/view_home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+    <base href="http://localhost/ci_intro/" />
+</head>
+<body>
+<a href="site/home">Home</a>
+<a href="site/about">About us</a>
+<br />
+<h1>Home</h1>
+</body>
+</html>
